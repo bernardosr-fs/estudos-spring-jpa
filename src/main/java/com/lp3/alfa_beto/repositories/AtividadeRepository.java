33e561f49747558fa6f50406316d62b812a76e57
Diogo Rodrigues <diogo2003rem@gmail.com>
2022-09-07 16:51:43 -0300

    adicionando repositories da atividade e medalha

--- src/main/java/com/lp3/alfa_beto/repositories/AtividadeRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/AtividadeRepository.java
@@ -1,5 +1,10 @@
 package com.lp3.alfa_beto.repositories;
 
-public class AtividadeRepository {
+import com.lp3.alfa_beto.entities.Atividade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
 
 }
--- src/main/java/com/lp3/alfa_beto/repositories/MedalhaRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/MedalhaRepository.java
@@ -1,5 +1,10 @@
 package com.lp3.alfa_beto.repositories;
 
-public class MedalhaRepository {
+import com.lp3.alfa_beto.entities.Medalha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedalhaRepository extends JpaRepository<Medalha, Long> {
 
 }
