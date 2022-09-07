46b2320f0a62c6019eac3a871500081b392a5fe3
Your Name <bernardosr@hotmail.com.br>
2022-09-07 16:52:44 -0300

    entidade de responsavel

--- src/main/java/com/lp3/alfa_beto/entities/Responsavel.java
+++ src/main/java/com/lp3/alfa_beto/entities/Responsavel.java
@@ -1,5 +1,29 @@
 package com.lp3.alfa_beto.entities;
 
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Responsavel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_responsavel")
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String cpf;
+
+    private String senha;
+
+    @ManyToMany
+    @JoinTable(name = "responsavel_estudante", joinColumns = @JoinColumn(name = "id_responsavel"), inverseJoinColumns = @JoinColumn(name = "id_estudante"))
+    private List<Estudante> estudantes;
 }
