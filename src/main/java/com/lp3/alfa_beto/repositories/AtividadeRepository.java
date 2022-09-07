5cd3ddd5f56a0a1604419ba16dd209f565f85afc
Your Name <bernardosr@hotmail.com.br>
2022-09-07 17:14:51 -0300

    Adicionando consulta a, b, c e d

--- src/main/java/com/lp3/alfa_beto/repositories/AtividadeRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/AtividadeRepository.java
@@ -1,5 +1,7 @@
 package com.lp3.alfa_beto.repositories;
 
+import java.util.List;
+
 import com.lp3.alfa_beto.entities.Atividade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
 
+    public List<Atividade> findAllByTituloOrDescricaoContainingIgnoreCase(String pesquisa);
+
+    public List<Atividade> findAllByIdEstudante(Long idEstudante);
 }
--- src/main/java/com/lp3/alfa_beto/repositories/EstudanteRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/EstudanteRepository.java
@@ -3,8 +3,13 @@ package com.lp3.alfa_beto.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.lp3.alfa_beto.entities.Estudante;
 
 @Repository
 public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
+
+    public List<Estudante> findAllByIdMedalha(Long idMedalha);
+
 }
