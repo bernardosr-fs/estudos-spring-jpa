51ae4ea0ae9f3c26756faa2c0b3197d1c631eab9
oAngulo-sh <andrewsmoehlecke@gmail.com>
2022-09-07 16:43:00 -0300

    Criando repositório para o estudante e o responsável

--- src/main/java/com/lp3/alfa_beto/repositories/EstudanteRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/EstudanteRepository.java
@@ -1,5 +1,10 @@
 package com.lp3.alfa_beto.repositories;
 
-public class EstudanteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.lp3.alfa_beto.entities.Estudante;
+
+@Repository
+public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
 }
--- src/main/java/com/lp3/alfa_beto/repositories/ResponsavelRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/ResponsavelRepository.java
@@ -1,5 +1,10 @@
 package com.lp3.alfa_beto.repositories;
 
-public class ResponsavelRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.lp3.alfa_beto.entities.Responsavel;
+
+@Repository
+public interface ResponsavelRepository extends JpaRepository<Responsavel, Long> {
 }
