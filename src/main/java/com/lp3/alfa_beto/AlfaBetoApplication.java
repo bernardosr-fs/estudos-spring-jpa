4d485fa7ddce9950cd3f2c3cd3e961c0debc7cee
oAngulo-sh <andrewsmoehlecke@gmail.com>
2022-09-07 15:27:20 -0300

    Add project files

---
+++ src/main/java/com/lp3/alfa_beto/AlfaBetoApplication.java
@@ -0,0 +1,13 @@
+package com.lp3.alfa_beto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaBetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaBetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lp3/alfa_beto/AlfaBetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp3.alfa_beto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaBetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
