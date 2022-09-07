33b5b035874683ae2fdcbcd066acd21152572f8f
Diogo Rodrigues <diogo2003rem@gmail.com>
2022-09-07 16:30:53 -0300

    Adicionando modelagem da Atividade e Medalha

--- src/main/java/com/lp3/alfa_beto/entities/Medalha.java
+++ src/main/java/com/lp3/alfa_beto/entities/Medalha.java
@@ -1,5 +1,23 @@
 package com.lp3.alfa_beto.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Medalha {
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id_medalha")
+    private Long id;
+
+    private String titulo;
 }
