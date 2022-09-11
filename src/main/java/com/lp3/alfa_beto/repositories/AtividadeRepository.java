6ca142f132bd3dd06adff17ea406af73c08db406
Diogo Rodrigues <diogo2003rem@gmail.com>
2022-09-11 15:38:38 -0300

    adicionando consulta A) e B)

--- src/main/java/com/lp3/alfa_beto/repositories/AtividadeRepository.java
+++ src/main/java/com/lp3/alfa_beto/repositories/AtividadeRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lp3.alfa_beto.entities.Atividade;
@@ -12,13 +13,12 @@ import com.lp3.alfa_beto.entities.Estudante;
 @Repository
 public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
 
-    public List<Atividade> findAllByTituloContainingIgnoreCase(String titulo);
+    //LETRA A)
+    List<Atividade> findAllByTituloOrDescricaoContainingIgnoreCase(String titulo, String descricao);
 
-    public List<Atividade> findAllByDescricaoContainingIgnoreCase(String descricao);
-
-    public List<Atividade> findAllById(Long id);
-
-    //public List<Atividade> findByDataDeAvaliacaoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
-
-    //public List<Atividade> findAllByEstudantesIn(List<Estudante> estudantes);
+    //LETRA B)
+    @Query("SELECT a FROM Atividade a " +
+            "WHERE a.id IN " +
+            "(SELECT ea.atividade.id FROM EstudanteAtividade ea WHERE ea.estudante.id = ?1)")
+    List<Atividade> findAllByIdEstudante(Long idEstudante);
 }
